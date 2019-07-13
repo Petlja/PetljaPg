1fc57d0bebe279f4f05a308b28a3a662f4d64e11
vasiljevic <nebojsa.vasiljevic@gmail.com>
2019-07-13 22:03:29 +0200

    manje izmene

--- primer3.py
+++ primer3.py
@@ -6,6 +6,7 @@ x = 150
 y = 150
 vx = 0
 vy = 0
+sirina, visina = 500, 300
 
 # crtanje možemo lako odvojiti u posebnu funkciju, ali na to
 # možemo da pređemo kada nam bude zaista zatrebalo, ne moramo
@@ -16,9 +17,7 @@ def crtaj(platno):
     boja = pg.Color("red")
     pg.draw.circle(platno, boja, (int(x), int(y)), 30)
 
-for frm in petljapg.frames(rate=30, size=(500,300)):
-    sirina, visina = frm.surface.get_size()
-
+for frm in petljapg.frames(rate=30, size=(sirina, visina)):
     vx = vx + frm.key_right_count() - frm.key_left_count()
     x = (x + vx * frm.period) % sirina
 
